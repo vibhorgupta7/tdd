dbfcbb79a12643dc14b6c2b9445fb28f8ce9c48c
vibhorgupta7 <vibhor4k@gmail.com>
2021-04-22 23:10:47 +0530

    Lecture 3 completed

--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,22 @@
 package co.interleap.courses.tdd;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void shouldCountZeroWithAnEmptyString(){
+        assertEquals(0, new VowelCounter().count(""));
+    }
+
+    @Test
+    public void shouldCountSingleVowel(){
+        assertEquals(1, new VowelCounter().count("u"));
+    }
+
+    @Test
+    public void shouldCountAllVowelsInStringHavingOnlyVowels(){
+        assertEquals(5, new VowelCounter().count("aeiou"));
+    }
 }
